5dd9428df455bf362a9210d6a5041a68c26f96b2
asadcse100 <asadcse100@gmail.com>
2023-12-19 01:45:43 +0600

    Affiliate add

--- resources/views/frontend/inc/footer.blade.php
+++ resources/views/frontend/inc/footer.blade.php
@@ -257,13 +257,11 @@
                                 {{ translate('Track Order') }}
                             </a>
                         </li>
-                        @if (addon_is_activated('affiliate_system'))
                             <li class="mb-2">
                                 <a class="fs-13 text-soft-light animate-underline-white" href="{{ route('affiliate.apply') }}">
-                                    {{ translate('Be an affiliate partner')}}
+                                    {{ translate('Business Affiliate Partner')}}
                                 </a>
                             </li>
-                        @endif
                     </ul>
                 </div>
             </div>
@@ -419,13 +417,11 @@
                                 {{ translate('Track Order') }}
                             </a>
                         </li>
-                        <!-- @if (addon_is_activated('affiliate_system')) -->
                             <li class="mb-2 pb-2 {{ areActiveRoutes(['affiliate.apply'],' active')}}">
                                 <a class="fs-13 text-soft-light text-sm-secondary animate-underline-white" href="{{ route('affiliate.apply') }}">
                                     {{ translate('Business Affiliate Partner')}}
                                 </a>
                             </li>
-                        <!-- @endif -->
                     </ul>
                 </div>
             </div>
